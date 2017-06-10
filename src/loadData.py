376f740c0f2af392887122b608ad1a4c5df2179b
Jarosław Steckiewicz <xitronix@mail.ru>
2017-06-10 12:59:01 +0200

    + add loadConfig in loadData

--- src/loadData.py
+++ src/loadData.py
@@ -8,3 +8,16 @@ class LoadData:
             row = line.split()
             localizations.add((float(row[0]),float(row[1])))
 
+        file.close()
+        return localizations
+
+    def readConfig(self, path):
+        file = open (path, 'r')
+        parameters = []
+
+        for line in file:
+            row = line.split()
+            parameters.append(row[0])
+
+        file.close();
+        return parameters

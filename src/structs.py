7d0b9c2fe59c92325b96816d104ebc345ee7bdfd
Sarnapa <piotrak.michal@gmail.com>
2017-06-10 17:16:21 +0200

    hotfix2

--- src/structs.py
+++ src/structs.py
@@ -1,7 +1,13 @@
+from src.utils import distanceBetweenPoints
+from random import sample
+import networkx as nx
+
 
 class RailNetworkTree:
 
     def __init__(self):
+
+    '''def __init__(self):
         self.cities_segments = []
         self.city_power_segments = []
         self.score = 0
@@ -18,9 +24,68 @@ class RailNetworkTree:
         self.score = 1.0 / self.score
         return self.score
 
-    def add_new_segment(self, line_segment):
-        self.segments.append(line_segment)
+    def add_new_segment(self, line_segment: LineSegment):
+        if line_segment.is_conn_to_power:
+            self.city_power_segments.append(line_segment)
+        else:
+            self.cities_segments.append(line_segment)
 
+    # to random one segment to get cycle
+    def random_new_segment(self):
+        insert_point = None
+        out_points = None
+        new_segment = None
+        neighbours_list = self.to_neighbours_list()
+        all_points = set(neighbours_list.keys())
+        unchosen_points = all_points.copy()
+        while unchosen_points:
+            insert_point = sample(unchosen_points, 1).pop()
+            unchosen_points.remove(insert_point)
+            neighbour_points = neighbours_list[insert_point]
+            out_points = (all_points - neighbour_points)
+            out_points.remove(insert_point)
+            if out_points:
+                break
+
+        # if there are some no "neighbour point", we create new segment.
+        if out_points:
+            new_neighbour = sample(out_points, 1).pop()
+            new_segment = LineSegment(insert_point, new_neighbour)
+            self.add_new_segment(new_segment)
+        else:
+            pass
+
+        return new_segment
+
+    # to create neighbours list that contains all neighbours for one index
+    def to_neighbours_list(self):
+        neighbours_list = {}
+        for seg in self.cities_segments:
+            points = seg.points.copy()
+            point1 = points.pop()
+            point2 = points.pop()
+            if point1 not in neighbours_list:
+                neighbours_list[point1] = set()
+            if point2 not in neighbours_list:
+                neighbours_list[point2] = set()
+            neighbours_list[point1].add(point2)
+            neighbours_list[point2].add(point1)
+        return neighbours_list
+
+    def find_cycle(self):
+        G = nx.Graph
+        nx.find_cycle()
+
+        return cycle
+
+    def get_cities_edges_list(self):
+        cities_edges_list = []
+        for seg in self.cities_segments:
+            points = seg.points.copy()
+            cities_edges_list.append(points)
+
+        return cities_edges_list
+    '''
 
 class LineSegment:
 
@@ -35,11 +100,8 @@ class LineSegment:
 
     def length(self):
         points = self.points.copy()
-        return distance(points.pop() + (0,), points.pop() + (0,))
+        return distanceBetweenPoints(points.pop() + (0,), points.pop() + (0,))
 
     def get_points(self):
         points_copy = self.points.copy()
         return points_copy
-
-    def get_is_conn_to_power(self):
-        return self.is_conn_to_power

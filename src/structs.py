28acc92f2238176b88dc267eca97b3a3524177f1
Sarnapa <piotrak.michal@gmail.com>
2017-06-11 11:57:57 +0200

    Improved print method and generating initial trees

--- src/structs.py
+++ src/structs.py
@@ -5,12 +5,15 @@ import networkx as nx
 
 class RailNetworkTree:
     def __init__(self, pos_dict: dict):
+        self.cities_nodes = set()
+        self.ps_nodes = set()
         self.graph = nx.Graph()
         for vertex, pos in pos_dict.items():
             self.graph.add_node(vertex, x=pos[0], y=pos[1])
-            #print(vertex, end=" ")
-            #print(self.get_node_x(vertex), end=" ")
-            #print(self.get_node_y(vertex))
+            if vertex < 0:
+                self.ps_nodes.add(vertex)
+            else:
+                self.cities_nodes.add(vertex)
 
     def get_node_x(self, vertex):
         return self.graph.node[vertex]['x']
@@ -19,10 +22,7 @@ class RailNetworkTree:
         return self.graph.node[vertex]['y']
 
     def get_node_pos(self, vertex):
-        pos = []
-        pos.append(self.get_node_x(vertex))
-        pos.append(self.get_node_y(vertex))
-        return pos #set(self.get_node_x(vertex), self.get_node_y(vertex)]
+        return self.get_node_x(vertex), self.get_node_y(vertex)
 
     def count_goal_fun(self, cost_traction, cost_add_electic_station):
         print("count goal func")
@@ -50,11 +50,11 @@ class RailNetworkTree:
         return
 
     def add_edge(self, v1, v2):
-        self.graph.add_edge(v1, v2) #weight=distanceBetweenPoints(self.get_node_pos(v1) + (0,), self.get_node_pos(v2) + (0,)))
+        self.graph.add_edge(v1, v2, weight=distanceBetweenPoints(self.get_node_pos(v1) + (0,), self.get_node_pos(v2) + (0,)))
 
     def generate_init_tree(self):
         used_nodes = set()
-        not_used_nodes = self.graph.nodes()
+        not_used_nodes = self.cities_nodes
         while len(not_used_nodes) != 0:
             node = sample(not_used_nodes, 1)
             node = node.pop(0)
@@ -62,25 +62,36 @@ class RailNetworkTree:
             if len(used_nodes) == 0:
                 used_nodes.add(node)
             else:
-                second_node = -1
-                # rand only cities
-                while second_node < 0:
-                    second_node = sample(used_nodes, 1)
-                    second_node = second_node.pop(0)
-
+                second_node = sample(used_nodes, 1)
+                second_node = second_node.pop(0)
                 self.add_edge(node, second_node)
                 used_nodes.add(node)
 
+        if len(used_nodes) != 0:
+            not_used_nodes = self.ps_nodes
+            while len(not_used_nodes) != 0:
+                node = sample(not_used_nodes, 1)
+                node = node.pop(0)
+                not_used_nodes.remove(node)
+                second_node = sample(used_nodes, 1)
+                second_node = second_node.pop(0)
+                # remove second_node because one city can be connected with one power station
+                used_nodes.remove(second_node)
+                self.add_edge(node, second_node)
+
+
     # na razie dla testow
     def print_tree(self):
+        print("Tree:")
+        print("Nodes:")
         for node in self.graph.nodes():
             print(node, end=" ")
-            print(self.get_node_x(node), end=" ")
-            print(self.get_node_y(node))
+            print("(", self.get_node_x(node), ",", self.get_node_y(node), ")")
         print("Edges:")
-        print(self.graph.edges())
+        for v1, v2 in self.graph.edges():
+            print(v1, "->", v2, "length: ", self.graph[v1][v2]['weight'])
 
-class LineSegment:
+'''class LineSegment:
     def __init__(self, p1, p2, conn=False):
         self.points = set()
         self.points.add(p1)
@@ -96,4 +107,4 @@ class LineSegment:
 
     def get_points(self):
         points_copy = self.points.copy()
-        return points_copy
+        return points_copy'''
